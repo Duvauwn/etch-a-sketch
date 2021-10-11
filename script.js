81699355de52cf46a3789fdb2f4f19b93637c884
Duvauwn Prendergast <duvauwnprendergast@gmail.com>
2021-10-11 19:33:05 +0100

    making it hover@ git status

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const container = document.querySelector('#container');
 
-
-
 for (let i = 0; i <= 255; i++) {
     const content = document.createElement('div');
     content.classList.add('content');
     content.style.border = '1px solid black';
     container.appendChild(content);
+
+    content.addEventListener('mouseover', () => {
+        content.classList.add('hovering');
+    });
+
 }
+
